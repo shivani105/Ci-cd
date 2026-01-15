66b6979dfeb8e0b9f468416a407b34d73057f3fa
shivani105 <shivanikrishnan123@gmail.com>
2026-01-15 13:46:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn more tuo,uyhnthjyuoi9ouyhthnyuo,i,iytrfrvn7lo8i
       </p>
     </>
   )
